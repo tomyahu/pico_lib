688ef452726f6cf0c6edd7d2282aef35731b5e8d
tomimi <tomyahu@gmail.com>
2023-01-23 14:22:44 -0300

    added soft copy

--- lib.lua
+++ lib.lua
@@ -8,3 +8,17 @@ function print_center(txt, x, y, c)
 	y -= 3
 	print(txt, x, y, c)
 end
+
+function s_copy( orig )
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		copy = {}
+		for orig_key, orig_value in pairs(orig) do
+			copy[orig_key] = orig_value
+		end
+	else -- number, string, boolean, etc
+		copy = orig
+	end
+	return copy
+end
