627f63636d9939b75437cff4294cebee0755e4cf
tomimi <tomyahu@gmail.com>
2023-01-23 16:14:33 -0300

    soft y deep copy

--- lib.lua
+++ lib.lua
@@ -2,6 +2,7 @@ function btoi(b)
 	return b and 1 or 0
 end
 
+
 function print_center(txt, x, y, c)
 	txt = tostr(txt);
 	x -= (#txt)*2
@@ -9,16 +10,28 @@ function print_center(txt, x, y, c)
 	print(txt, x, y, c)
 end
 
-function s_copy( orig )
+
+function soft_copy( orig )
 	local orig_type = type(orig)
-	local copy
-	if orig_type == 'table' then
-		copy = {}
-		for orig_key, orig_value in pairs(orig) do
-			copy[orig_key] = orig_value
-		end
-	else -- number, string, boolean, etc
-		copy = orig
+	if orig_type != 'table' then return orig end
+
+	local copy = {}
+	for orig_key, orig_value in pairs(orig) do
+		copy[orig_key] = orig_value
 	end
+
+	return copy
+end
+
+
+function deep_copy( orig )
+	local orig_type = type(orig)
+	if orig_type != 'table' then return orig end
+
+	local copy = {}
+	for orig_key, orig_value in pairs(orig) do
+		copy[orig_key] = deep_copy( orig_value )
+	end
+
 	return copy
 end
