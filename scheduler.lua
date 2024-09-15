b05ce9108614ad6c7afecc8a8522dd756a3b383e
tomimi <tomyahu@gmail.com>
2024-09-14 22:34:00 -0300

    bugfixes for the scheduler

--- scheduler.lua
+++ scheduler.lua
@@ -3,13 +3,15 @@ function new_scheduler()
 		current_time = 0,
 		scheduled_functions={},
 		schedule_function= function(self, func, frames)
-			local time_hash = "" .. frames
+			local time_hash = "" .. (frames+self.current_time)
 			if self.scheduled_functions[time_hash] == nil then
 				self.scheduled_functions[time_hash] = {}
 			end
 			add(self.scheduled_functions[time_hash], func)
 		end,
 		update=function(self)
+			self.current_time += 1
+			
 			local time_hash = "" .. self.current_time
 			if self.scheduled_functions[time_hash] != nil then
 				for func in all(self.scheduled_functions[time_hash]) do
@@ -18,7 +20,6 @@ function new_scheduler()
 				self.scheduled_functions[time_hash] = nil
 			end
 
-			self.current_time += 1
 		end,
 		clear=function(self)
 			self.scheduled_functions = {}
