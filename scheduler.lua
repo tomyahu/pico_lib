1fc003ed1f0ef7c68fcaba3cc516ef689e7ae032
tomimi <tomyahu@gmail.com>
2024-09-14 22:20:47 -0300

    added new scheduler file

---
+++ scheduler.lua
@@ -0,0 +1,24 @@
+function new_scheduler()
+	return {
+		current_time = 0,
+		scheduled_functions={},
+		schedule_function= function(self, func, frames)
+			local time_hash = "" .. frames
+			if self.scheduled_functions[time_hash] == nil then
+				self.scheduled_functions[time_hash] = {}
+			end
+			add(self.scheduled_functions[time_hash], func)
+		end,
+		update=function(self)
+			local time_hash = "" .. self.current_time
+			if self.scheduled_functions[time_hash] != nil then
+				for func in all(self.scheduled_functions[time_hash]) do
+					func()
+				end
+				self.scheduled_functions[time_hash] = nil
+			end
+
+			self.current_time += 1
+		end
+	}
+end
