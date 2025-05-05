b4b475ac50fa56a8e324a8062c5c7aaa3f075735
tomimi <tomyahu@gmail.com>
2025-05-05 01:14:10 -0400

    functions to encode/decode numbers to a string

---
+++ string_to_table.lua
@@ -0,0 +1,116 @@
+START_STR = 97
+BYTE_SIZE = 5
+
+
+function get_bit(number, i)
+	return (number >> i) & 1
+end
+
+
+function string_to_numbers( string )
+	local res = {}
+
+	for i=1,#string do
+		add(
+			res,
+			mid(
+				0,
+				ord(
+					charat(string, i)
+				) - START_STR,
+				make_mask(1, BYTE_SIZE)
+			)
+		)
+	end
+
+	return res
+end
+
+
+function numbers_to_string( numbers )
+	local res = ""
+
+	for _, n in pairs(numbers) do
+		res ..= chr(n + START_STR)
+	end
+
+	return res
+end
+
+
+-- make_mask: int, int ->
+-- makes a mask of continious 1's from right to left
+-- from the start bit i to the end bit j
+function make_mask(i, j)
+	if j == nil then j = i end
+	local mask = 0
+	for index=i, j do
+		mask += 1 << index
+	end
+	return mask
+end
+
+
+function get_from_number( number, i, j )
+	return number & make_mask(i, j)
+end
+
+
+function get_from_numbers( bytes, i, j )
+	local start_index = (i \ BYTE_SIZE) + 1
+	local end_index = (j \ BYTE_SIZE) + 1
+	local res = 0
+	local shift_val = - (i % BYTE_SIZE)
+	for index = start_index, end_index do
+		local byte = bytes[index]
+		local bit_pos = ( index - 1 ) * BYTE_SIZE
+		res += get_from_number(
+			byte,
+			mid(0, i - bit_pos, BYTE_SIZE - 1),
+			mid(0, j - bit_pos, BYTE_SIZE - 1)
+		) << shift_val
+		shift_val += BYTE_SIZE
+	end
+	return res
+end
+
+
+function decode_from_string(string, keys)
+	local numbers = string_to_numbers(string)
+	local res = {}
+	local acc = 0
+	for _, key in pairs(keys) do
+		add( res, get_from_numbers(numbers, acc, acc + key - 1))
+		acc += key
+	end
+
+	return res
+end
+
+
+function encode_to_string( array, keys )
+	local arr_index = 1
+	local acc = 0
+	local byte_num = 0
+	local res = ""
+	for _, k in pairs(keys) do
+		for i=1,k do
+			arr_val = array[arr_index]
+
+			acc += get_bit(arr_val, i-1) << byte_num
+			byte_num += 1
+
+			if byte_num == BYTE_SIZE then
+				res ..= chr( START_STR + acc )
+				acc = 0
+				byte_num = 0
+			end
+		end
+		arr_index += 1
+	end
+
+	if byte_num > 0 then
+		res ..= chr( START_STR + acc )
+	end
+	return res
+end
